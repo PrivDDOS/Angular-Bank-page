173b825bb6bf8106bc3faf52cd88ea352b2bb2b8
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-03-04 11:23:35 +0700

    fixing src can not reach the assets folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponents } from './components/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponents
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponents } from "./components/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainComponents],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
