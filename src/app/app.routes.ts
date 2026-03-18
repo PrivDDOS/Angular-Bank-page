b719283b7157aaf6f70b5d0477ff920dc644336e
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-03-18 11:36:19 +0700

    Create About component and added the route also finish the Homepage responsive mobile CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainComponents } from './components/main/main';
+import { AboutComponents } from './components/about/about';
 
 export const routes: Routes = [
     {
         path: '',
         component: MainComponents
+    },
+    {
+        path: 'about',
+        component: AboutComponents
     }
 ];
