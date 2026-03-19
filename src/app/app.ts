4c13a8e4d7556e84a06e3520975c8c936f810fa7
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-03-19 11:40:32 +0700

    Create About page and added router link to Nav button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponents } from "./components/main/main";
+import { AboutComponents } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [MainComponents],
+  imports: [MainComponents, AboutComponents, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
